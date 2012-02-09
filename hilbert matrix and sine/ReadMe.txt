652be7c7830a095abe932420056b547ea679e898
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-02-09 04:57:34 +0200

    Added old VS2008 project to mercurial. (after renaming it to 'hilbert matrix and sine' from 'Benchmark CLR Console sine')
    Original timestamp (from hilbert matrix and sine.sln): 5/11/2008 11:18:28am
    Original timestamp (from hilbert matrix and sine.cpp): 5/11/2008  3:49:24pm
    
    --HG--
    branch : C++/CLI CLR Console

---
+++ hilbert matrix and sine/ReadMe.txt
@@ -0,0 +1,28 @@
+========================================================================
+    APPLICATION : hilbert matrix and sine Project Overview
+========================================================================
+
+AppWizard has created this hilbert matrix and sine Application for you.  
+
+This file contains a summary of what you will find in each of the files that
+make up your hilbert matrix and sine application.
+
+hilbert matrix and sine.vcproj
+    This is the main project file for VC++ projects generated using an Application Wizard. 
+    It contains information about the version of Visual C++ that generated the file, and 
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+hilbert matrix and sine.cpp
+    This is the main application source file.
+
+AssemblyInfo.cpp
+	Contains custom attributes for modifying assembly metadata.
+
+/////////////////////////////////////////////////////////////////////////////
+Other notes:
+
+AppWizard uses "TODO:" to indicate parts of the source code you
+should add to or customize.
+
+/////////////////////////////////////////////////////////////////////////////
---
+++ hilbert matrix and sine/hilbert matrix and sine.cpp
@@ -0,0 +1,114 @@
+// hilbert matrix and sine.cpp : main project file.
+
+#include "stdafx.h"
+
+/*
+ * file main.cpp
+ * created by Nightmare
+ * on 5 Íïå 2008
+ */
+
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::endl;
+
+using std::ofstream;
+
+using std::setprecision;
+
+using std::fabs;
+
+using std::clock;
+
+
+class Test
+{
+public:
+	static double sine(register double x)
+	{
+		register double sum = x;
+		register double term = x;
+		register int i = 2;
+
+		x = -x*x;
+		//cout<<x<<endl;
+		term = x*term / ((i + 1)*i);
+
+		while(fabs(term/sum) > 1e-15/*fabs(term) > 1e-9*/)
+		{
+			sum += term;
+			//cout<<sum<<endl;
+			i += 2;
+			term = (x*term) / ((i + 1)*i);
+		} // end while
+
+		return sum;
+	}
+
+	static void hilb(double *A , unsigned int order)
+	{
+		register double *p;
+		register double *q;
+		register unsigned int c,i,n;
+		double x;
+
+		//c = order*order;	// but the # of elements in the matrix in c.
+		p = A;	// set p at the beginning of A.
+
+		c = 0;
+		i = 2;
+		*p = 1.0;	// set the first element of A to 1.
+
+		while(++c != order)
+		{
+			*(q = ++p) = x = 1.0/i;
+			++i;
+			n = c;
+			while(n--)
+			{
+				*(q += order-1) = x;
+			}
+		}											//times to compute hilb(5000):
+		while(--c)									//with Microsoft C++ compiler (Native):
+		{											//Debug: 422ms	Release: 344ms	Optimized: 281ms
+			*(q = p += order) = x = 1.0/i;
+			++i;									//with Microsoft C++ compiler (Managed):
+			n = c;									//Debug: 390ms or 375	Release: 329ms	Optimized: 328ms
+			while(--n)
+			{										//with gcc 390ms 390 or 375 optimized
+				*(q += order-1) = x;
+			}
+		}
+
+	} // end fuction hilb
+}; // end class Test
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+	clock_t outTime , inTime;
+	ofstream outFile("c:/stats.txt");	// variables required for measuring and storing the results.
+
+	unsigned int order = 0;	// variables needed for the program itself.
+	double *A;
+
+	outTime = clock();
+
+	for(order = 1000 ; order <= 6000 ; order += 20)
+	{
+		A = new double[order*order];
+		inTime = clock();
+		Test::hilb(A,order);
+		outFile<<clock()-inTime<<endl;
+		delete[] A;
+	}
+
+	outFile<<"\n\n"<<clock()-outTime<<endl;
+
+
+
+	//system("PAUSE");
+	return 0;
+} // end function main
